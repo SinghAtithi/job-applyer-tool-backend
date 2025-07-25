2fb75204c60e081530bb125a365a2bf6a9ca4572
Atithi Kumar Singh <mails4rockstararyan@gmail.com>
2025-07-25 21:34:21 +0530

    Removed unused ID fields from Resume, Education, Experience, and Project structs

--- internal/models/resume.go
+++ internal/models/resume.go
@@ -33,7 +33,6 @@ import (
 
 // Resume represents a user's resume, including metadata and comprehensive resume data.
 type Resume struct {
-	ID          uint      `json:"id" gorm:"primaryKey"`                                                          // Unique identifier for the resume
 	UserID      uuid.UUID `json:"user_id" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"` // Associated user ID
 	Title       string    `json:"title"`                                                                         // Title of the resume
 	Description string    `json:"description,omitempty"`                                                         // Optional description
@@ -83,7 +82,6 @@ type Address struct {
 
 // Education represents an educational qualification.
 type Education struct {
-	ID           uint       `json:"id,omitempty"`             // Unique identifier for the education entry
 	Institution  string     `json:"institution"`              // Name of the institution
 	Degree       string     `json:"degree"`                   // Degree or certification obtained
 	FieldOfStudy string     `json:"field_of_study,omitempty"` // Field of study or major
@@ -96,7 +94,6 @@ type Education struct {
 
 // WorkExperience represents a work experience entry.
 type Experience struct {
-	ID               uint       `json:"id,omitempty"`                                       // Unique identifier for the experience entry
 	JobTitle         string     `json:"job_title"`                                          // Job title
 	Company          string     `json:"company"`                                            // Company name
 	Role             string     `json:"role,omitempty"`                                     // Optional role description
@@ -132,7 +129,6 @@ type TechnicalSkill struct {
 
 // Project represents a project entry.
 type Project struct {
-	ID           uint       `json:"id,omitempty"`                                  // Unique identifier for the project
 	Name         string     `json:"name"`                                          // Project name
 	Description  string     `json:"description,omitempty"`                         // Optional project description
 	Technologies []string   `json:"technologies,omitempty" gorm:"serializer:json"` // List of technologies used
