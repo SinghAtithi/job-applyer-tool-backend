c4ae5da9f5895ca4508dd58787fc84f641d92228
Atithi Kumar Singh <mails4rockstararyan@gmail.com>
2025-07-26 19:05:19 +0530

    Renamed the resume model and added cover_letter_handler.go

--- api/handler/cover_letter_handler.go
+++ api/handler/cover_letter_handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	_ "gorm.io/gorm"
+	grom "gorm.io/gorm"
+)
+
+type CoverLetterHandler struct {
+	db *grom.DB
+}
+
+func NewCoverLetter(db *grom.DB) *CoverLetterHandler {
+	return &CoverLetterHandler{
+		db: db,
+	}
+}
